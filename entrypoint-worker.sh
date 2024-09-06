084af2f0626238cac5c3000adfa0279ec252a799
Robert Timm <mail@rtti.de>
2024-09-06 07:10:47 +0000

    style: format

--- entrypoint-worker.sh
+++ entrypoint-worker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # TODO: do we need a nanny port?
-python -m dask worker dask-scheduler.rtti.de:8786 --contact-address tcp://${PUBLIC_IPADDR}:${VAST_TCP_PORT_3001} --listen-address tcp://0.0.0.0:3001
+python -m dask worker \
+  dask-scheduler.rtti.de:8786 \
+  --contact-address tcp://${PUBLIC_IPADDR}:${VAST_TCP_PORT_3001} \
+  --listen-address tcp://0.0.0.0:3001
